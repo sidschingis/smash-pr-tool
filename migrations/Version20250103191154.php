c9305e363bfd083445bf4480005f0a05b0acd4f1
sidschingis <2721785+sidschingis@users.noreply.github.com>
2025-01-29 22:49:26 +0100

    fix: fix Migrations

--- migrations/Version20250103191154.php
+++ migrations/Version20250103191154.php
@@ -20,17 +20,12 @@ final class Version20250103191154 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE placement DROP CONSTRAINT fk_48db750e71f7e88b');
-        $this->addSql('DROP INDEX idx_48db750e71f7e88b');
         $this->addSql('ALTER TABLE player ALTER tag TYPE VARCHAR(50)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE player ALTER tag TYPE VARCHAR(20)');
-        $this->addSql('ALTER TABLE placement ADD CONSTRAINT fk_48db750e71f7e88b FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('CREATE INDEX idx_48db750e71f7e88b ON placement (event_id)');
     }
 }
--- migrations/Version20250103212614.php
+++ migrations/Version20250103212614.php
@@ -26,7 +26,6 @@ final class Version20250103212614 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE event ALTER region TYPE VARCHAR(10)');
     }
 }
