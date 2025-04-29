89c6fbf6fff0aeaff1800050fc2e7086c2eaa70b
sidschingis <2721785+sidschingis@users.noreply.github.com>
2025-04-29 23:32:19 +0200

    feat: Update Wins Losses View
    Order by tier
    Adjusted formatting

--- src/Queries/Ranking/FetchWinsLosses.php
+++ src/Queries/Ranking/FetchWinsLosses.php
@@ -111,11 +111,20 @@ class FetchWinsLosses
 
         $result = [];
         foreach ($data as $tier => $headToHeads) {
-            $result[] = new ResultContainer(
+            $order = match($tier) {
+                'S' => 0,
+                'A' => 1,
+                'B' => 2,
+                'C' => 3,
+                'D' => 4,
+            };
+            $result[$order] = new ResultContainer(
                 $tier,
                 $headToHeads
             );
         }
+
+        ksort($result);
 
         return $result;
     }
